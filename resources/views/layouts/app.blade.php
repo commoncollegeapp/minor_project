0e00d6641ef394aeed6045cafcdc68074e01ee06
sushant-bhattarai <sushantb2055@gmail.com>
2020-11-07 13:14:00 +0545

    created Profile model and resourceful controller

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -23,18 +23,41 @@
     <div id="app">
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
-                </a>
+                @guest
+                    <a class="navbar-brand" href="{{ url('/') }}">
+                        <img src="images/logo.jpg" alt="Common College App">
+                        <!-- {{ config('app.name', 'Laravel') }} -->
+                    </a>
+                @else
+                    <a class="navbar-brand" href="{{ url('/home') }}">
+                        <img src="images/logo.jpg" alt="Common College App">
+                        <!-- {{ config('app.name', 'Laravel') }} -->
+                    </a>
+                @endguest
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
                 </button>
 
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <!-- Left Side Of Navbar -->
-                    <ul class="navbar-nav mr-auto">
-
+                    @guest
+                    @else
+                    <ul class="navbar-nav mr-auto font-weight-bold">
+                        <a href="{{ url('/home') }}" class="nav-link">Dashboard</a>
                     </ul>
+                    <ul class="navbar-nav mr-auto font-weight-bold">
+                        <a href="#" class="nav-link">My Profile</a>
+                    </ul>
+                    <ul class="navbar-nav mr-auto font-weight-bold">
+                        <a href="#" class="nav-link">My Colleges</a>
+                    </ul>
+                    <ul class="navbar-nav mr-auto font-weight-bold">
+                        <a href="#" class="nav-link">My Applications</a>
+                    </ul>
+                    <ul class="navbar-nav mr-auto font-weight-bold">
+                        <a href="#" class="nav-link">Apply to a College</a>
+                    </ul>
+                    @endguest
 
                     <!-- Right Side Of Navbar -->
                     <ul class="navbar-nav ml-auto">
